535e895f84498e71b268547c911fb6be41aa7ae5
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-08-24 15:55:01 +0530

    Router

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connect } from "mongoose";
+import cookieParser from "cookie-parser";
 import connectDB from "./src/db/connect.js";
+import userRoute from "./src/routes/userRoute.js";
 
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", userRoute)
 
 const server = async () => {
   try {
---
+++ backend/src/routes/userRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const userRoute = express.Router();
+
+userRoute.post("/register",);
+userRoute.post("/register",);
+userRoute.get("/logout",);
+userRoute.get("/user",);
+userRoute.patch("/user",);
+
+userRoute.delete("/admin/user/:id",);
+userRoute.post("/forgot-password",);
+userRoute.post("/reset-password",);
+userRoute.post("/change-password",);
+
+export default userRoute;
