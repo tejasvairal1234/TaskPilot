14c77f675fbaadbaef358d115141f47424a2a5f5
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-08-30 22:21:41 +0530

    Validator Login Middleware

--- backend/src/validators/authValidator.js
+++ backend/src/validators/authValidator.js
@@ -18,3 +18,15 @@ export const registerSchema = z.object({
     .min(8, "Password must be at least 8 characters")
     .max(100, "Password cannot exceed 100 characters"),
 });
+
+export const loginSchema = z.object({
+    email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .email("Please provide a valid email"),
+
+    password: z
+    .string()
+    .min(8, "Password is required"),
+})
