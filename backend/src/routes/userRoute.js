87353fc776aff0fb83a03ed390f9888824b0c6da
Tejas Vairal <tejasvairal7066@gmail.com>
2026-09-06 23:39:39 +0530

    Add more API

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,13 +1,32 @@
 import express from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/auth/userController.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  getMe,
+  updateUser,
+  deleteUser,
+} from "../controllers/auth/userController.js";
 import validate from "../middlewares/validateMiddleware.js";
-import { loginSchema, registerSchema } from "../validators/authValidator.js";
+import protect from "../middlewares/protectMiddleware.js";
+import {
+  loginSchema,
+  registerSchema,
+  updateUserSchema,
+} from "../validators/authValidator.js";
 
 const userRoute = express.Router();
 
+// Public routes
 userRoute.post("/register", validate(registerSchema), registerUser);
 userRoute.post("/login", validate(loginSchema), loginUser);
 userRoute.post("/refresh", refreshAccessToken);
 userRoute.post("/logout", logoutUser);
+
+// Protected routes
+userRoute.get("/me", protect, getMe);
+userRoute.put("/me", protect, validate(updateUserSchema), updateUser);
+userRoute.delete("/me", protect, deleteUser);
 
 export default userRoute;
