7c1a5da8423f52cac6130fd728250efc93a75c1c
Tejas Vairal <tejasvairal7066@gmail.com>
2026-09-06 11:18:57 +0530

    Update TaskPilot

---
+++ backend/src/models/auth/RefreshTokenModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const refreshTokenSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    token: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RefreshToken = mongoose.model(
+  "RefreshToken",
+  refreshTokenSchema
+);
+
+export default RefreshToken;
