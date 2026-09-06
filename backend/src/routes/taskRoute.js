f2e471b2d39935a9b448afb650b569554110b6a7
Tejas Vairal <tejasvairal7066@gmail.com>
2026-09-06 23:37:44 +0530

    Add more API

--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -1,16 +1,32 @@
 import express from "express";
 import protect from "../middlewares/protectMiddleware.js";
-import { createTask, deleteTask, getTaskByTitle, getTasks, updateTask } from "../controllers/task/taskController.js";
-// import { validate } from "zod";
+import {
+  createTask,
+  deleteTask,
+  getDashboardStats,
+  getTaskById,
+  getTasks,
+  updateTask,
+} from "../controllers/task/taskController.js";
 import { createTaskSchema, updateTaskSchema } from "../validators/taskValidator.js";
 import validate from "../middlewares/validateMiddleware.js";
 
 const taskRoute = express.Router();
 
-taskRoute.post("/create", protect, validate(createTaskSchema), createTask);
-taskRoute.get("/", protect, getTasks);
-taskRoute.get("/search", protect, getTaskByTitle);
-taskRoute.patch("/update/:id", protect, validate(updateTaskSchema), updateTask);
-taskRoute.delete("/delete/:id", protect, deleteTask);
+// All task routes require authentication
+taskRoute.use(protect);
+
+// Dashboard stats
+taskRoute.get("/dashboard", getDashboardStats);
+
+// RESTful task CRUD
+taskRoute.route("/")
+  .get(getTasks)
+  .post(validate(createTaskSchema), createTask);
+
+taskRoute.route("/:id")
+  .get(getTaskById)
+  .put(validate(updateTaskSchema), updateTask)
+  .delete(deleteTask);
 
 export default taskRoute;
